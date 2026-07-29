f47b02e452617492596996826ce5f3140e8cadff
heiicha <joanlo682@gmail.com>
2026-07-29 15:59:23 +0800

    update link

--- src/components/feedback.jsx
+++ src/components/feedback.jsx
@@ -5,7 +5,7 @@ import "./feedback.css"
 function FeedbackButton() {
     return (
         <div> 
-            <Link to='https://www.youtube.com/watch?v=mpa781T8F5U'>
+            <Link to='https://docs.google.com/forms/d/e/1FAIpQLSdhZHgLVDqmxbv2ajIzxnea7_JZeL53OvICwaKFD7dUWK0iEA/viewform?usp=sharing&ouid=111965143662204677500'>
             <button className="feedback-button"> Have any feedback? </button>
             </Link>
         </div>
